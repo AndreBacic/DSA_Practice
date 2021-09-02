8c5db4bdc4b0660a7ae2c34b7bb23cbf2eb87d52
andre9050@yahoo.com <andre9050@yahoo.com>
2021-09-02 11:22:18 -0400

    array assignments 2+3

--- arrayPractice1.py
+++ arrayPractice1.py
@@ -1,6 +1,41 @@
 from typing import List
 
 class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if digits[len(digits)-1] >= 9:
+            return self.carryOverNum(len(digits)-1, digits)
+        digits[len(digits)-1] += 1
+        return digits
+
+    def carryOverNum(self, i: int, digits: List[int]) -> List[int]:
+        if i < 0:
+            digits = [1]+digits
+        elif digits[i] >= 9:
+            digits[i] = 0
+            digits = self.carryOverNum(i-1, digits)
+        else:
+            digits[i] += 1
+        return digits
+
+
+    def dominantIndex(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+        largest = -1
+        s_largest = -1
+        index = -1
+        for i, v in enumerate(nums):
+            if v > largest:
+                s_largest = largest
+                largest = v
+                index = i
+            elif v > s_largest:
+                s_largest = v
+
+        if largest >= s_largest*2:
+            return index
+        return -1
+
     def pivotIndex(self, nums: List[int]) -> int:
         output = -1
 
