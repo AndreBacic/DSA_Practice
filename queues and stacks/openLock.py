a42e06bd64f01b8c9aa93aad834157c473e205bf
andre9050@yahoo.com <andre9050@yahoo.com>
2021-09-06 15:43:15 -0400

    Another working algorithm that takes too long because using BFS means scanning through huge lists way too many times.

---
+++ queues and stacks/openLock.py
@@ -0,0 +1,48 @@
+from typing import List
+import time
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if "0000" in deadends:
+            return -1
+        
+
+        
+    def openLockWithBFS(self, deadends: List[str], target: str) -> int:
+        """
+        Slow as molasses because it has to search through a couple huge lists a bajillion times.
+        """
+        if "0000" in deadends:
+            return -1
+        queue = ["0000"]
+        visited = ["0000"]
+        step = -1
+
+        while (len(queue) > 0):
+            step += 1
+            size = len(queue)
+            for i in range(size):
+                current = queue.pop(0)
+                if current == target:
+                    return step
+                for i in range(4):
+                    for b in [True, False]:
+                        newCombo = self.turnLock(current, i, b)
+                        if newCombo not in deadends and newCombo not in visited:
+                            queue.append(newCombo)
+                            visited.append(newCombo)
+                
+        return -1
+    
+    def turnLock(self, currentCombo: str, pos: int, forward: bool = True) -> str:
+        s = int(currentCombo[pos])
+        if forward:
+            s = (s + 1)%10
+        else:
+            s = (s - 1)%10
+        return currentCombo[0:pos]+ str(s) + currentCombo[pos+1:]
+
+
+s = Solution()
+t = time.time()
+print(s.openLock(["5557","5553","5575","5535","5755","5355","7555","3555","6655","6455","4655","4455","5665","5445","5645","5465","5566","5544","5564","5546","6565","4545","6545","4565","5656","5454","5654","5456","6556","4554","4556","6554"], "5555"))
+print(time.time()-t)
