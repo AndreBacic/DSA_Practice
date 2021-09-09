8a0ad43b543386c1ca19d60df45bd3cb0db7a95e
andre9050@yahoo.com <andre9050@yahoo.com>
2021-09-09 11:30:43 -0400

    N-ary tree practice 1

---
+++ N-ary trees/preorder_traversal.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root: return []
+        
+        output = []
+
+        output.append(root.val)
+        for c in root.children:
+            output += self.preorder(c)
+
+        return output
