22ac0cd5d318b5548e5402d7b4decd95b8587de1
andre9050@yahoo.com <andre9050@yahoo.com>
2022-04-12 22:26:42 -0400

    SQL string manipulation challenges

---
+++ sql challenges/Group Sold Products By The Date.sql
@@ -0,0 +1,14 @@
+Create table Activities (sell_date date, product varchar(20))
+Truncate table Activities
+insert into Activities (sell_date, product) values ('2020-05-30', 'Headphone')
+insert into Activities (sell_date, product) values ('2020-06-01', 'Pencil')
+insert into Activities (sell_date, product) values ('2020-06-02', 'Mask')
+insert into Activities (sell_date, product) values ('2020-05-30', 'Basketball')
+insert into Activities (sell_date, product) values ('2020-06-01', 'Bible')
+insert into Activities (sell_date, product) values ('2020-06-02', 'Mask')
+insert into Activities (sell_date, product) values ('2020-05-30', 'T-Shirt')
+
+SELECT sell_date, COUNT(product) AS num_sold, 
+        STRING_AGG(product, ',') WITHIN GROUP (ORDER BY product ASC) AS products
+ FROM (SELECT DISTINCT(product), sell_date FROM Activities) AS T
+  GROUP BY sell_date ORDER BY sell_date ASC;
