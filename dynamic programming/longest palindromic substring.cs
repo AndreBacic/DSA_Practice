79c01b62bec56616dbf440a4825ab78b075d21f4
andre9050@yahoo.com <andre9050@yahoo.com>
2022-04-26 21:29:41 -0400

    Debugged O(n) time palindrome algorithm

--- dynamic programming/longest palindromic substring.cs
+++ dynamic programming/longest palindromic substring.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 public class Solution0 {
     /// <summary>
     /// My original idea but the variables l2 and l1 are actually unnecessary.
@@ -99,66 +102,96 @@ public class Solution {
     }
 }
 
+
 public class ManachersAlgorithm {
-    public string LongestPalindrome(string s) {
-        // uses Manacher's algorithm to find the longest palindrome in a string
-        // O(n) time and O(n) space        
-        // https://en.wikipedia.org/wiki/Longest_palindromic_substring#Manacher's_algorithm
-        // NOTE: This code was generated with GitHub Copilot.
-        // TODO: THIS CODE DOES NOT WORK AND MUST BE DEBUGGED AT SOME POINT
-
-        if (s == null || s.Length == 0) {
-            return "";
+    /// <summary>
+    /// Manacher's algorithm, taken from Wikipedia and transpiled from psuedocode to C#.
+    /// O(n) time and O(n) space.
+    ///
+    /// Source url:
+    /// https://en.wikipedia.org/wiki/Longest_palindromic_substring#Manacher's_algorithm
+    /// </summary>
+    public string LongestPalindrome(string S) {
+        // S with a bogus character (eg. '|') inserted between each character (including outer boundaries)
+        string s = "|" + string.Join("|", S.Select(c => c.ToString())) + "|";
+        int[] PalindromeRadii = new int[s.Length]; // The radius of the longest palindrome centered on each place in s
+        // note: length(s) = length(PalindromeRadii) = 2 × length(S) + 1
+        
+        int Center = 0;
+        int Radius = 0;
+        
+        while (Center < s.Length) {
+            // At the start of the loop, Radius is already set to a lower-bound for the longest radius.
+            // In the first iteration, Radius is 0, but it can be higher.
+            
+            // Determine the longest palindrome starting at Center-Radius and going to Center+Radius
+            while (Center-(Radius+1) >= 0 && Center+Radius+1 < s.Length && s[Center-(Radius+1)] == s[Center+Radius+1]) {
+                Radius++;
+            }             
+         
+            // Save the radius of the longest palindrome in the array
+            PalindromeRadii[Center] = Radius;
+            
+            // Below, Center is incremented.
+            // If any precomputed values can be reused, they are.
+            // Also, Radius may be set to a value greater than 0
+            
+            int OldCenter = Center;
+            int OldRadius = Radius;
+            Center++;
+            // Radius' default value will be 0, if we reach the end of the following loop. 
+            Radius = 0;
+            while (Center <= OldCenter + OldRadius) {
+                // Because Center lies inside the old palindrome and every character inside
+                // a palindrome has a "mirrored" character reflected across its center, we
+                // can use the data that was precomputed for the Center's mirrored point. 
+                int MirroredCenter = OldCenter - (Center - OldCenter);
+                int MaxMirroredRadius = OldCenter + OldRadius - Center;
+                if (PalindromeRadii[MirroredCenter] < MaxMirroredRadius) {
+                    PalindromeRadii[Center] = PalindromeRadii[MirroredCenter];
+                    Center++;
+                }   
+                else if (PalindromeRadii[MirroredCenter] > MaxMirroredRadius) {
+                    PalindromeRadii[Center] = MaxMirroredRadius;
+                    Center++;
+                }   
+                else { // PalindromeRadii[MirroredCenter] = MaxMirroredRadius
+                    Radius = MaxMirroredRadius;
+                    break; // exit while loop early
+                }   
+            }      
         }
-        if (s.Length == 1) {
-            return s;
-        }
-        int maxLen = 0;
-        int start = 0;
-        string T = preProcess(s);
-        int[] P = new int[T.Length];
-        int C = 0;
-        int R = 0;
-        for (int i = 1; i < T.Length - 1; i++)
-        {
-            int i_mirror = 2 * C - i;
-            P[i] = (R > i) ? Math.Min(R - i, P[i_mirror]) : 0;
-            // Attempt to expand palindrome centered at i
-            while (T[i + 1 + P[i]] == T[i - 1 - P[i]])
-            {
-                P[i]++;
-            }
-            // If palindrome centered at i expand past R,
-            // adjust center based on expanded palindrome.
-            if (i + P[i] > R)
-            {
-                C = i;
-                R = i + P[i];
-            }
-            // Find the maximum element in P.
-            if (P[i] > maxLen)
-            {
-                maxLen = P[i];
-                start = (i - C) / 2;
+        int maxRadius = -1;
+        int maxRadiusIndex = -1;
+        for (int i = 0; i < PalindromeRadii.Length; i++) {
+            if (PalindromeRadii[i] > maxRadius) {
+                maxRadius = PalindromeRadii[i];
+                maxRadiusIndex = i/2;
             }
         }
-        return s.Substring(start, maxLen);
+        
+        int longest_palindrome_in_s = 2*maxRadius+1;
+        // longest_palindrome_in_s = 2*max(PalindromeRadii)+1;
+        int longest_palindrome_in_S = (longest_palindrome_in_s - 1)/2; // = PalindromeRadii.Max() == maxRadius
+        
+        //Console.WriteLine($"{maxRadius} {maxRadiusIndex}    {S}    {s}");
+        string output = S.Substring(maxRadiusIndex - longest_palindrome_in_S/2, longest_palindrome_in_S);
+        return output;
     }
+}
 
-    private string preProcess(string s)
+class Program
+{
+    static void Main(string[] args)
     {
-        // Adds a '^' and an '$' to the beginning and end of the string.
-        // This is done so that the string is not treated as a substring of itself.
-        // O(n) time and O(n) space.
-        // https://en.wikipedia.org/wiki/Longest_palindromic_substring#Manacher's_algorithm
-        // NOTE: This code was generated with GitHub Copilot.
-
-        string T = "^";
-        for (int i = 0; i < s.Length; i++)
-        {
-            T += "#" + s[i];
-        }
-        T += "#$";
-        return T;
+        //Solution s = new Solution();
+        ManachersAlgorithm m = new ManachersAlgorithm();
+        string test1 = "babad";
+        string test2 = "cbbd";
+        string test3 = "abacdfgdcaba";
+        //Console.WriteLine(s.LongestPalindrome(test1));
+        Console.WriteLine(m.LongestPalindrome(test1));
+        Console.WriteLine(m.LongestPalindrome(test2));
+        Console.WriteLine(m.LongestPalindrome(test3));
     }
 }
