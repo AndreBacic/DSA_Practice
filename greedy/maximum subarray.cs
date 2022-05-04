727f65da74dcfa45bcaa71d8d76bf28437d2b5a5
andre9050@yahoo.com <andre9050@yahoo.com>
2022-05-04 19:26:52 -0400

    Greedily calculate maximum subarray

--- greedy/maximum subarray.cs
+++ greedy/maximum subarray.cs
@@ -1,18 +1,16 @@
 public class Solution {
+    /// <summary>
+    /// Find the maximum contiguous subarray of non negative numbers from an array.
+    /// O(n) time and O(1) space.
+    /// </summary>
     public int MaxSubArray(int[] nums) {
-        if (nums.Length == 1) {
-            return nums[0];
-        }
-        // brute force!
+        if (nums.Length == 1) return nums[0];
         int largest = Int32.MinValue;
+        int sum = 0;
         for (int i = 0; i < nums.Length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.Length; j++) {
-                sum += nums[j];
-                if (sum > largest) {
-                    largest = sum;
-                }
-            } 
+            sum += nums[i];
+            if (sum > largest) largest = sum;
+            if (sum < 0) sum = 0; 
         }
         return largest;
     }
