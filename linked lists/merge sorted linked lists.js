984521781741455814e3917caefa039b223a76f7
andre9050@yahoo.com <andre9050@yahoo.com>
2022-05-06 17:39:01 -0400

    Less verbose algorithm

--- linked lists/merge sorted linked lists.js
+++ linked lists/merge sorted linked lists.js
@@ -4,16 +4,43 @@ function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
 }
+
+/**
+ * Merge two sorted linked lists and return it as a new list (head of new list is output)
+ * 
+ * O(n) time and O(1) space 
+ * @param {ListNode} list1 
+ * @param {ListNode} list2 
+ * @returns {ListNode}
+ */
+const mergeTwoLists = function(list1, list2) {
+    let head = new ListNode()
+    let tail = head
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            tail.next = list1
+            list1 = list1.next
+        } else {
+            tail.next = list2
+            list2 = list2.next
+        }
+        tail = tail.next
+    }
+    tail.next = list1 || list2
+    return head.next // head is the dummy node, so head.next is the first real node
+}
  
 /**
  * Merge two sorted linked lists and return it as a new list (head of new list is output)
+ * This solution that I submitted is more verbose than needed. The use of a dummy node, while not necessary,
+ * simplifies the code.
  * 
  * O(n) time and O(1) space
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
- const mergeTwoLists = function(list1, list2) {
+ const mergeTwoListsSubmitted = function(list1, list2) {
     if (!list1) {
         return list2
     }
