1feb8f791bc94af33145fa5dc0dba4d305ef7017
andre9050@yahoo.com <andre9050@yahoo.com>
2022-05-07 22:58:34 -0400

    Alternative solution

--- backtracking/subset.cs
+++ backtracking/subset.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 public class Solution {
     public IList<IList<int>> Subsets(int[] nums) {
         // ALL numbers in nums are unique
@@ -14,5 +17,40 @@ public class Solution {
             }            
         }
         return output;
+    }
+    /// <summary>
+    /// Backtracking solution
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public IList<IList<int>> Subsets_with_backtracking(int[] nums) {
+        IList<IList<int>> output = new List<IList<int>>();
+        backtrack(output, new List<int>(), nums, 0);
+        return output;
+    }
+    private void backtrack(IList<IList<int>> output, List<int> curr, int[] nums, int start) {
+        if (start == nums.Length) {
+            output.Add(new List<int>(curr));
+            return;
+        }
+        backtrack(output, curr, nums, start + 1);
+        curr.Add(nums[start]);
+        backtrack(output, curr, nums, start + 1);
+        curr.RemoveAt(curr.Count - 1);
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 2, 3};
+        IList<IList<int>> output = s.Subsets_with_backtracking(nums);
+        foreach (IList<int> list in output) {
+            Console.Write("[  ");
+            foreach (int n in list) {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine(" ]");
+        }
     }
 }
