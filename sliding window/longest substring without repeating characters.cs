a5f2dbb7ef5ee600ff9fc88ab37419ad3cb601d2
andre9050@yahoo.com <andre9050@yahoo.com>
2022-05-09 13:59:35 -0400

    Sliding window refactor

--- sliding window/longest substring without repeating characters.cs
+++ sliding window/longest substring without repeating characters.cs
@@ -4,6 +4,23 @@ public class Solution {
         if (s.Length == 0) return 0;
         
         int largest = 0;
+        int left = -1;
+        Dictionary<char, int> table = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++) {
+            char c = s[i];
+            if (table.ContainsKey(c) && table[c] >= left) {
+                left = table[c];
+            }
+            table[c] = i;
+            largest = Math.Max(i-left, largest);
+        }
+        return largest;
+    }
+    public int LengthOfLongestSubstringWithDictReset(string s) {
+        // O(n) time, O(1) space best case, O(n) space worst
+        if (s.Length == 0) return 0;
+        
+        int largest = 0;
         int tally = 0;
         Dictionary<char, int> table = new Dictionary<char, int>();
         for (int i = 0; i < s.Length; i++) {
