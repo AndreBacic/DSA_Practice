844c5789644a1724a71d99c769aff2bf7768e817
andre9050@yahoo.com <andre9050@yahoo.com>
2022-06-04 17:59:10 -0400

    Optimized banana eating solution!

--- binary search/Koko eating bananas.cs
+++ binary search/Koko eating bananas.cs
@@ -1,28 +1,32 @@
 public class Solution
 {
     /// <summary>
-    /// Unoptimized O(n*m) time, O(1) space where m is the largest value in piles and n is piles.Length
-    /// First sums the piles and computes the minimum speed if all piles are equal, and then increments min speed until all the piles can be consumed in h hours.
+    /// O( n * log(m) ) time (worst case), O(1) space.   (where m is the largest value in piles and n is piles.Length)
+    /// First sums the piles and computes the minimum speed if all piles are equal and memoizes the max pile, 
+    /// and then binary searches between the minimum speed and the max pile to find the minimum speed, iterating through the piles each time.
     /// </summary>
     /// <param name="piles"></param>
     /// <param name="h"></param>
     /// <returns></returns>
-    public int MinEatingSpeed0(int[] piles, int h)
+    public int MinEatingSpeed(int[] piles, int h)
     {
         // h is always >= piles.length so there is always a solution
         double s = 0;
-        foreach (int p in piles) s += p;
-        int m = (int)Math.Ceiling(s / h);
-        while (true)
+        int k = 0;
+        foreach (int p in piles) { s += p; k = Math.Max(k, p); }
+        int j = (int)Math.Ceiling(s / h);
+        int found = -1;
+        while (j <= k)
         {
+            int m = (j + k) / 2;
             int l = h;
             for (int i = 0; i < piles.Length; i++)
             {
                 l -= (int)Math.Ceiling((double)piles[i] / m);
             }
-            if (l >= 0) break;
-            m++;
+            if (l >= 0) { found = m; k = m - 1; }
+            else j = m + 1;
         }
-        return m;
+        return found;
     }
 }
