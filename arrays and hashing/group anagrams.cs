ecbb1b0f671551b93e7dc92cf1a86ba264915018
andre9050@yahoo.com <andre9050@yahoo.com>
2022-06-04 01:57:12 -0400

    Optimized GroupAnagrams for scaling complexity

--- arrays and hashing/group anagrams.cs
+++ arrays and hashing/group anagrams.cs
@@ -6,7 +6,7 @@ public class Solution
     /// </summary>
     /// <param name="strs"></param>
     /// <returns></returns>
-    public IList<IList<string>> GroupAnagrams(string[] strs)
+    public IList<IList<string>> GroupAnagrams0(string[] strs)
     {
         List<IList<string>> output = new();
 
@@ -25,5 +25,29 @@ public class Solution
         }
 
         return output;
+    }
+
+    /// <summary>
+    /// Since strs[i] will contain only lowercase English letters, we can use a 26-element array to represent the count of each letter.
+    /// 
+    /// Where n is strs.Length, and m is the average strs[i].Length
+    /// O(n m) time, O(n m) space
+    /// </summary>
+    /// <param name="strs"></param>
+    /// <returns></returns>
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, IList<string>> d = new();
+        foreach (string s in strs)
+        {
+            int[] a = new int[26];
+            foreach (char c in s) a[c - 'a']++;
+            // convert a to a string because arrays are reference types
+            string t = string.Join(",", a);
+            if (d.ContainsKey(t)) d[t].Add(s);
+            else d.Add(t, new List<string>() { s });
+        }
+
+        return d.Values.ToList();
     }
 }
