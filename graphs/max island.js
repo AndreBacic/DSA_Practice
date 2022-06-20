54e4c5a8645b3a95ee3a0e874f5eddbca6df4ffb
andre9050@yahoo.com <andre9050@yahoo.com>
2022-06-20 00:19:17 -0400

    Working solution to max island

--- graphs/max island.js
+++ graphs/max island.js
@@ -1,29 +1,30 @@
 /**
  * @param {number[][]} grid
  * @return {number}
+ * O(m*n) time, O(m*n) max space
+ * 
+ * Note that the recursive version of this solution has the same complexity, but apparently runs faster.
  */
 const maxAreaOfIsland = function (grid) {
-    // m*n time, m*n max space
-    // traverse until a 1 is found
-    // total++; add all neighbor 1's to stack; change current to 0
-    // repeat until no neighbors; largest = total; total = 0
-    // repeat last 3 until end of grid
-    let m = grid.length, n = grid[0].length, l = 0
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
+    let m = grid.length - 1, n = grid[0].length - 1, l = 0
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
             if (grid[i][j] !== 1) continue
 
             let t = 0
             const s = [[i, j]]
 
             while (s.length > 0) {
-                t++
                 c = s.pop()
+                if (grid[c[0]][c[1]] === 1) t++
                 grid[c[0]][c[1]] = 0
 
                 if (c[0] > 0 && grid[c[0] - 1][c[1]] === 1) s.push([c[0] - 1, c[1]])
+                if (c[0] < m && grid[c[0] + 1][c[1]] === 1) s.push([c[0] + 1, c[1]])
+                if (c[1] > 0 && grid[c[0]][c[1] - 1] === 1) s.push([c[0], c[1] - 1])
+                if (c[1] < n && grid[c[0]][c[1] + 1] === 1) s.push([c[0], c[1] + 1])
             }
-
+            l = Math.max(l, t)
         }
     }
     return l
